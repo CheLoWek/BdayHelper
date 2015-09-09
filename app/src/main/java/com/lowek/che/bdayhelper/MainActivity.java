5dadc87341c8420f12bb4615b085c0c96a6df9ad
CheLoWek <che.nadya.lw@gmail.com>
2015-09-09 16:46:15 +0300

    Added Nav drawer.

--- app/src/main/java/com/lowek/che/bdayhelper/MainActivity.java
+++ app/src/main/java/com/lowek/che/bdayhelper/MainActivity.java
@@ -12,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         initToolbar();
